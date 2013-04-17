f51843460cdd1c1f7423966169d5676508f51c6d
inca <incarnate@whiteants.net>
2013-04-17 11:25:34 +0400

    Project created.

---
+++ src/main/scala/main.scala
@@ -0,0 +1,10 @@
+package pro.savant
+package site
+
+import circumflex._, core._, web._, freemarker._
+
+class Main extends Router {
+
+
+
+}
---
+++ src/main/scala/package.scala
@@ -0,0 +1,11 @@
+package pro.savant
+
+import pro.savant.circumflex._, core._
+
+package object site {
+
+  val log = new Logger("pro.savant.site")
+
+
+
+}
